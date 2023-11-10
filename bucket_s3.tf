6ef2d2ca96061cb4ecfe4c7564cc51b17447ba23
Nicolas Abihaggle <90660792+rabihaggle@users.noreply.github.com>
2023-11-10 21:40:50 +0100

    update tf

--- bucket_s3.tf
+++ bucket_s3.tf
@@ -6,3 +6,6 @@ resource "aws_s3_bucket" "bucket" {
 output "bucket_name" {
   value = aws_s3_bucket.bucket.bucket
 }
+
+
+
--- providers.tf
+++ providers.tf
@@ -109,12 +109,12 @@ terraform {
 
   # Only allow this Terraform version. Note that if you upgrade to a newer version, Terraform won't allow you to use an
   # older version, so when you upgrade, you should upgrade everyone on your team and your CI servers all at once.
-  required_version = "~> 1.6.2"
+  required_version = "~> 1.5.2"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.0.0"
+      version = ">= 5.25.0"
     }
   }
 }
